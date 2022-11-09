692c089b673cd61169adf08ccf907c645562e699
21schizoidman <santiagocoradeghini@hotmail.com>
2022-11-09 20:50:38 -0300

    agrego funcion para buscar pasajes

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const main = async () => {
+  const buttonLading = document.getElementById('button-landing')
+  buttonLading.addEventListener('click', () => window.location = "/pages/destinos.html")
+}
+document.addEventListener("DOMContentLoaded", main)
