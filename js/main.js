7605e84c45b0561859765177bb4024c399b271a1
21schizoidman <santiagocoradeghini@hotmail.com>
2022-11-14 19:42:04 -0300

    nose

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const main = async () => {
-  const buttonLading = document.getElementById('button')
+  const buttonLading = document.getElementById('button-destinos')
   buttonLading.addEventListener('click', () => window.location = "/pages/destinos.html")
 }
 document.addEventListener("DOMContentLoaded", main)
